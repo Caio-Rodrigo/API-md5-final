b7f382f1ad1c804035995b702a5b13ac87d9b19c
Caio Rodrigo Silva <78828999+Caio-Rodrigo@users.noreply.github.com>
2022-11-10 21:30:18 -0300

    ok

--- build/server.js
+++ build/server.js
@@ -3,4 +3,4 @@ import router from "./routes.js";
 const app = express();
 app.use(express.json());
 app.use(router);
-app.listen(3333, () => console.log("Servidor rodadndo na porta 3333"));
+app.listen(3333, () => console.log("Servidor rodadndo na porta http://localhost:3333/user"));
--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,4 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.listen(3333, () => console.log("Servidor rodadndo na porta 3333"));
+app.listen(3333, () => console.log("Servidor rodadndo na porta http://localhost:3333/user"));
